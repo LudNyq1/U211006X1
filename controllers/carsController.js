5def09b23a9dbc48245bb2e7f0115b41a9ebca2f
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-10-07 10:06:34 +0200

    Fixed problem when reading cars.json

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -3,11 +3,13 @@ exports.index = (req, res) => {
 };
 
 exports.car_by_id = (req, res) => {
-    const cars = require('../public/data/cars.json');
+    const cars_data = require('../public/data/cars.json');
+    const cars = cars_data['cars'];
 
     let car_data = cars.find( x => {
         return x.Id == req.params.Id;
     });
+
     //Converts car_data into document readable raw text
     var data = 'ID: ' + car_data.Id + ' Make: ' + car_data.Make + ' Model: ' + 
     car_data.Model + ' Color: ' + car_data.Color+ ' Km: ' + 
